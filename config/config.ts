fee2e1135324b37af2bf91da0d3e92764caa3adf
Xifo <xifo.wu@gmail.com>
2023-03-14 14:01:01 +0800

    chore: 抽离出路由文件

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@umijs/max';
+import routes from './routes';
 
 export default defineConfig({
   antd: {},
@@ -18,58 +19,6 @@ export default defineConfig({
   },
   title: '影巢 - HD Hive',
   layout: {},
-  routes: [
-    {
-      path: '/user',
-      layout: false,
-      routes: [
-        {
-          path: '/user/login',
-          name: '影巢登录',
-          component: './Login',
-        },
-      ],
-    },
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/',
-      flatMenu: true,
-      routes: [
-        {
-          name: '首页',
-          hideInMenu: true,
-          path: '/home',
-          component: './Home',
-        },
-        {
-          name: '资源分享',
-          icon: 'cloud',
-          path: '/share',
-          component: './Share',
-        },
-        {
-          name: '用户管理',
-          icon: 'team',
-          path: '/users',
-          component: './User',
-        },
-        {
-          name: '流派管理',
-          icon: 'nodeIndex',
-          path: '/genre',
-          component: './Genre',
-        },
-        {
-          name: '系统配置',
-          icon: 'setting',
-          path: '/system-setting',
-          component: './SystemSeting',
-        },
-      ],
-    },
-  ],
+  routes,
   npmClient: 'pnpm',
 });
