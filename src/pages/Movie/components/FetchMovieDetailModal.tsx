21c05a3b912a19084fc000530b6e501288e27e15
Xifo <xifo.wu@gmail.com>
2023-03-23 00:01:56 +0800

    feat: 已存在时打开分享列表 Modal

--- src/pages/Movie/components/FetchMovieDetailModal.tsx
+++ src/pages/Movie/components/FetchMovieDetailModal.tsx
@@ -1,10 +1,11 @@
-import { Button, Card, Col, Modal, Row } from 'antd';
+import { Button, Card, Col, message, Modal, Row } from 'antd';
 import { ReactNode, useState } from 'react';
 import { ProForm, ProFormText } from '@ant-design/pro-components';
 import useModal from '@/lib/hooks/useModal';
 import tmdbAltShortIcon from '@/assets/tmdb_alt_short_icon.svg';
 import { getTmdbDetail } from '@/services/tmdb';
 import type { Movie } from '@/services/types/movie';
+import api from '@/lib/utils/api';
 
 interface Props {
   trigger?: ReactNode;
@@ -21,13 +22,46 @@ const FetchMovieDetailModal = ({
     setSelectedMode(modeName);
   };
 
+  const handleIsExist = async (key: string, value: any) => {
+    try {
+      const response = await api.get<any, any>(
+        '/api/v1/manager/movies-is-exist',
+        {
+          params: {
+            [key]: value,
+          },
+        },
+      );
+
+      return response;
+    } catch (error: any) {
+      message.error(error.message);
+      return;
+    }
+  };
+
   const handleTmdbFinish = async (values: any) => {
     const params: Movie = await getTmdbDetail(values.url);
     if (!params) {
       return;
     }
 
-    openModal('CreateMovieModalForm', params);
+    const response = await handleIsExist('tmdb_id', params.id);
+    if (!response) return;
+
+    const {
+      meta: { is_exist: isExist },
+      data,
+    } = response;
+
+    if (typeof isExist !== 'boolean') return;
+
+    if (isExist) {
+      openModal('ResourcesModal', { slug: data.slug });
+    } else {
+      openModal('CreateMovieModalForm', params);
+    }
+
     closeModal(modalName);
   };
 
